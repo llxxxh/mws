f1f248e7e38f2104e0e726b792efc41198d36790
llxxxh <lxh793238656@qq.com>
2024-02-01 21:29:15 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "MWS",
+        short_name: "MWS",
+        description: "My MWS App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "gb-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "gb-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
